adaaa88a4a3531af9195752bafecaeb5bdbc405f
Broche <xuym@360cbs.com>
2024-07-20 15:32:34 +0800

    导出功能优化

--- XAutoAuction_Data.lua
+++ XAutoAuction_Data.lua
@@ -1,4 +1,6 @@
 XAutoAuctionDataFrame = CreateFrame('Frame')
+XAutoAuctionData = {}
+local moduleName = 'XAutoAuctionData'
 
 XAuctionInfoList = {}
 XAuctionInfoListImport = ''
@@ -47,32 +49,38 @@ end
 local function export()
     if not XBuyExport or XBuyExport == '' then
         local buyJsonList = {}
+        local count = #XBuyList
         for _, item in ipairs(XBuyList) do
             table.insert(buyJsonList,
                 format('{"itemname":"%s","time":%s,"price":%s,"count":%s}',
-                    item.itemName, item.time, item.price, item.count))
+                    item.itemname, item.time, item.price, item.count))
         end
         XBuyExport = format('[%s]', table.concat(buyJsonList, ','))
         XBuyList = {}
+        print('Export buy history: ' .. count)
     end
 
     if not XSellExport or XSellExport == '' then
         local sellJsonList = {}
+        local count = #XSellList
         for _, item in ipairs(XSellList) do
             table.insert(sellJsonList,
                 format('{"itemname":"%s","time":%s,"issuccess":%s,"price":%s,"count":%s}',
-                    item.itemName, item.time, item.isSuccess and 'true' or 'false', item.price, item.count))
+                    item.itemname, item.time, item.issuccess and 'true' or 'false', item.price, item.count))
         end
         XSellExport = format('[%s]', table.concat(sellJsonList, ','))
         XSellList = {}
+        print('Export sell history: ' .. count)
     end
 
     if not XScanExport or XScanExport == '' then
         local scanJsonList = {}
+        local count = 0
         for itemName, item in pairs(XScanList) do
             local listJsonList = {}
             for _, record in ipairs(item['list']) do
                 table.insert(listJsonList, format('{"time":%s,"price":%s}', record.time, record.price))
+                count = count + 1
             end
             table.insert(scanJsonList,
                 format('"%s":{"timestamp":%s,"itemid":%s,"vendorprice":%s,"category":"%s","class":"%s","list":[%s]}',
@@ -81,21 +89,22 @@ local function export()
         end
         XScanExport = format('{%s}', table.concat(scanJsonList, ','))
         XScanList = {}
+        print('Export scan history: ' .. count)
     end
 end
 
 XAutoAuctionDataFrame:RegisterEvent('ADDON_LOADED')
-XAutoAuctionDataFrame:RegisterEvent('PLAYER_INTERACTION_MANAGER_FRAME_HIDE')
-XAutoAuctionDataFrame:RegisterEvent('AUCTION_HOUSE_CLOSED')
 
 XAutoAuctionDataFrame:SetScript('OnEvent', function(self, event, text, content)
     if event == 'ADDON_LOADED' then
         if text == 'XAutoAuction_Data' then
             import()
         end
-    elseif event == 'PLAYER_INTERACTION_MANAGER_FRAME_HIDE' then
-        if text == Enum.PlayerInteractionType.MailInfo then export() end
-    elseif event == 'AUCTION_HOUSE_CLOSED' then
-        export()
     end
 end)
+
+-- Commands
+SlashCmdList['XAUTOAUCTIONDATAEXPORT'] = function()
+    export()
+end
+SLASH_XAUTOAUCTIONDATAEXPORT1 = '/xautoauctiondata_export'
